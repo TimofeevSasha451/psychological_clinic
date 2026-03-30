1086fce5cc1485196d63c2c4eafb88a4c5ad225e
daytly <nfrcf123@xmail.ru>
2026-03-30 21:44:44 +0700

    fix

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+python manage.py migrate --noinput
+python manage.py collectstatic --clear --noinput
+
+exec gunicorn tobacco_shop_backend.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 60
